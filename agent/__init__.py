6e5d822a03bfdf11d5a88c28867529d5cf80977b
Carl <2063893484@qq.com>
2026-03-22 16:12:00 +0800

    feat: initial PersonalMind project - 你的私人AI大脑

---
+++ agent/__init__.py
@@ -0,0 +1,22 @@
+"""
+PersonalMind Agent Package
+"""
+from agent.core import PersonalMindAgent, get_agent, reset_agent
+from agent.memory import MemorySystem, get_memory, reset_memory
+from agent.llm import LLMInterface, get_llm, reset_llm
+from agent.tools import ToolRegistry, get_tools, reset_tools
+
+__all__ = [
+    "PersonalMindAgent",
+    "get_agent",
+    "reset_agent",
+    "MemorySystem",
+    "get_memory",
+    "reset_memory",
+    "LLMInterface",
+    "get_llm",
+    "reset_llm",
+    "ToolRegistry",
+    "get_tools",
+    "reset_tools",
+]
